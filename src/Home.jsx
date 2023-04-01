fdc640a2ba7d60c85c68d035c7f521584569564d
SHAJAN29 <m.j.sandy2916@gmail.com>
2023-04-01 11:06:24 +0530

    Press me

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,18 @@
 import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
-import "./Home.css"
+import "./Home.css";
+import { useNavigate } from "react-router-dom";
 export function Home() {
+  const navigate = useNavigate();
   return (
     <div className="homePage">
-
       <Typography variant="h1" sx={{ color: "GrayText" }}>
-        welcome to the react shajan😍
+        welcome to the CRUD App😍
       </Typography>
-      <Button variant="contained">Text</Button>
-
+      <Typography variant="p">Have fun</Typography>
+      <Button variant="contained" onClick={() => navigate("/Addusers")}>
+        press me
+      </Button>
     </div>
-
-
   );
 }
